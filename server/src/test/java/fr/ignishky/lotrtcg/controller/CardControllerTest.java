04770e7da90725c13c516794f6de5188a96e0ee8
Thierry DUCLOYER <Ignishky@users.noreply.github.com>
2020-04-04 17:09:11 +0200

    [Site] Add site configuration. (#1)

---
+++ server/src/test/java/fr/ignishky/lotrtcg/controller/CardControllerTest.java
@@ -0,0 +1,29 @@
+package fr.ignishky.lotrtcg.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CardControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void should_get_card_by_id() throws Exception {
+        mvc.perform(get("/cards/1"))
+                .andExpect(status().is(SC_OK))
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(content().json("{\"id\":\"1\", \"name\":\"Frodo\"}"));
+    }
+}
